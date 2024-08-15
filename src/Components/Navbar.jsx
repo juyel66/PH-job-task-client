77904548594ceb2651c06177078c48322a1da03f
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-08-15 18:15:29 +0600

    search and dropdown button added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { toast } from "react-toastify";
 import { AuthContext } from "./AuthProvider";
+import { FaGoogle } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 
 const Navbar = () => {
@@ -21,7 +23,35 @@ const Navbar = () => {
 
     const Links = (<>
 
+
+<details className="dropdown mr-5 lg:flex hidden">
+  <summary className="btn m-1 text-xl"><GiHamburgerMenu className="mt-1 text-2xl" /> Categories</summary>
+  <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</details>
+
+<label className="hidden input input-bordered border-gray-800 rounded-full lg:w-[500px] lg:flex items-center gap-2 mr-5">
+  <input type="text" className="grow" placeholder="Search" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      fillRule="evenodd"
+      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+      clipRule="evenodd" />
+  </svg>
+</label>
+
         <li> <NavLink className={({ isActive }) => isActive ? "btn btn-outline btn-success text-bold text-xl" : "text-xl font-bold"} to="/" > Home </NavLink> </li>
+        <li> <NavLink className={({ isActive }) => isActive ? "btn btn-outline btn-success text-bold text-xl" : "text-xl font-bold"} to="/about" > About </NavLink> </li>
+       
+      
+        <li> <NavLink className={({ isActive }) => isActive ? "btn btn-outline btn-success text-bold text-xl" : "text-xl font-bold"} to="/profile" > Profile </NavLink> </li>
+        <li> <NavLink className={({ isActive }) => isActive ? "btn btn-outline btn-success text-bold text-xl" : "text-xl font-bold"} to="/contact" > Contact </NavLink> </li>
 
 
 
@@ -59,7 +89,7 @@ const Navbar = () => {
 
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">ShopSmart</a>
+                    <a className="btn btn-ghost text-3xl">ShopSmart</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
